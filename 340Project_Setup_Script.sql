e1b2e377578f95d189500bd37cbd95df4cfa2dd9
Chopgi <128537431+Chopgi@users.noreply.github.com>
2026-05-09 21:14:15 -0500

    Added MySQL DB setup script

---
+++ 340Project_Setup_Script.sql
@@ -0,0 +1,32 @@
+create schema its340ProjectDB;
+USE its340ProjectDB;
+
+CREATE TABLE patients (
+  patientID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  lastName VARCHAR(50),
+  firstName VARCHAR(50),
+  age INT,
+  gender VARCHAR(10) DEFAULT NULL,
+  blood_type VARCHAR(3) DEFAULT NULL);
+  
+CREATE TABLE allergy_history (
+  AllergyID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  patientID INT,
+  Allergen VARCHAR(50));  
+  
+CREATE TABLE immunization_history (
+  immunizationID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  patientID INT,
+  immunization VARCHAR(50));    
+  
+CREATE TABLE medication_history (
+  medicationID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  patientID INT,
+  medication VARCHAR(50));   
+  
+CREATE TABLE hereditary_diseases_history (
+  hereditary_diseaseID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  patientID INT,
+  hereditary_disease VARCHAR(50));   
+
+ SELECT * FROM its340ProjectDB.patients;
