40ade1534aab2a68940d40f60d3a85b9d2069b8a
Chopgi <chop10009@gmail.com>
2026-05-10 00:59:10 -0500

    fixed warning by commenting out unused import

--- src/PatientInfo.java
+++ src/PatientInfo.java
@@ -4,7 +4,7 @@ import java.awt.event.*;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
+//import java.sql.ResultSet;
 import java.sql.Statement;
 
 public class PatientInfo {
